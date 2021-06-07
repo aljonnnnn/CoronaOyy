d4d815e7b4e32694644404cd80edfda25a257562
aljonnnnn <delumenaljon@gmail.com>
2021-06-07 18:09:25 +0800

    Add variable action types

--- src/contexts/action/coronaInfoAction.js
+++ src/contexts/action/coronaInfoAction.js
@@ -1,8 +1,10 @@
+import { GET_COUNTRIES, GET_COUNTRY_DATA, GET_GLOBAL_DATA, SET_HISTORY, SET_HISTORY_ERROR, SET_LOADING } from "../types/coronaInfoTypes"
+
 export const getCountryName = async(dispatch) => {
     fetch('https://disease.sh/v3/covid-19/countries')
     .then(response => response.json())
     .then(data => {
-        dispatch({type: 'GET_COUNTRIES', payload: data.map(data => data.country)})
+        dispatch({type: GET_COUNTRIES, payload: data.map(data => data.country)})
     })
     .catch(error => {
         console.log(error)
@@ -13,9 +15,9 @@ export const getGlobalData = async(dispatch) => {
     fetch('https://disease.sh/v3/covid-19/all')
     .then(response => response.json())
     .then(data => {
-        dispatch({type: 'SET_LOADING'})
+        dispatch({type: SET_LOADING})
         setTimeout(() => {
-            dispatch({type: 'GET_GLOBAL_DATA', payload: {cases: data.cases, deaths: data.deaths, recovered: data.recovered, active: data.active}})
+            dispatch({type: GET_GLOBAL_DATA, payload: {cases: data.cases, deaths: data.deaths, recovered: data.recovered, active: data.active}})
         }, 500)
     })
     .catch(error => {
@@ -27,9 +29,9 @@ export const getCountryData = async(dispatch, country) => {
     fetch('https://disease.sh/v3/covid-19/countries/' + country)
     .then(response => response.json())
     .then(data => {
-        dispatch({type: 'SET_LOADING'})
+        dispatch({type: SET_LOADING})
         setTimeout(()  => {
-            dispatch({ type: 'GET_COUNTRY_DATA', payload: {cases: data.cases, deaths: data.deaths, recovered: data.recovered, active: data.active, flag: data.countryInfo.flag}})
+            dispatch({ type: GET_COUNTRY_DATA, payload: {cases: data.cases, deaths: data.deaths, recovered: data.recovered, active: data.active, flag: data.countryInfo.flag}})
         }, 500)
     })
     .catch(error => {
@@ -51,19 +53,19 @@ export const getHistoryData = async(dispatch, country) => {
             throw new Error('Country not found or doesn\'t have any historical data')
         } else if (data.timeline) {
             setTimeout(() => {
-                dispatch({type: 'SET_HISTORY', payload: {cases: data.timeline.cases, deaths: data.timeline.deaths, recovered: data.timeline.recovered, message: null}})
+                dispatch({type: SET_HISTORY, payload: {cases: data.timeline.cases, deaths: data.timeline.deaths, recovered: data.timeline.recovered, message: null}})
             }, 500)
         } else {
             setTimeout(() => {
-                dispatch({type: 'SET_HISTORY', payload: {cases: data.cases, deaths: data.deaths, recovered: data.recovered, message: null}})
+                dispatch({type: SET_HISTORY, payload: {cases: data.cases, deaths: data.deaths, recovered: data.recovered, message: null}})
             }, 500)
         }
     })
     .catch(error => {
         // console.log(error)
-        dispatch({type: 'SET_HISTORY_ERROR', payload: error.message})
+        dispatch({type: SET_HISTORY_ERROR, payload: error.message})
         setTimeout(() => {
-            dispatch({type: 'SET_HISTORY', payload: { cases: 0, deaths: 0, recovered: 0, message: error.message } })
+            dispatch({type: SET_HISTORY, payload: { cases: 0, deaths: 0, recovered: 0, message: error.message } })
         }, 500)
     })
 }
--- src/contexts/reducer/coronaInfoReducer.js
+++ src/contexts/reducer/coronaInfoReducer.js
@@ -1,3 +1,5 @@
+import { CHANGE_COUNTRY_SELECT, GET_COUNTRIES, GET_COUNTRY_DATA, GET_GLOBAL_DATA, SET_HISTORY, SET_HISTORY_ERROR, SET_LOADING } from "../types/coronaInfoTypes"
+
 export const coronaInfoState = {
     countriesName: [],
     country: null,
@@ -11,43 +13,43 @@ export const coronaInfoState = {
 
 export const coronaInfoReducer = (state, {type, payload}) => {
     switch(type) {
-        case 'GET_COUNTRIES' :
+        case GET_COUNTRIES :
             return {
                 ...state,
                 countriesName: payload,
             }
-        case 'GET_GLOBAL_DATA' :
+        case GET_GLOBAL_DATA :
             return {
                 ...state,
                 country: null,
                 global: payload,
                 loading: false,
             }
-        case 'GET_COUNTRY_DATA' :
+        case GET_COUNTRY_DATA :
             return {
                 ...state,
                 global: null,
                 country: payload,
                 loading: false,
             }
-        case 'CHANGE_COUNTRY_SELECT' :
+        case CHANGE_COUNTRY_SELECT :
             return {
                 ...state,
                 countrySelected: payload,
                 historyError: null
 
             }
-        case 'SET_LOADING' :
+        case SET_LOADING :
             return {
                 ...state,
                 loading: true
             }
-        case 'SET_HISTORY' :
+        case SET_HISTORY :
             return {
                 ...state,
                 history : payload,
             }
-        case 'SET_HISTORY_ERROR' : 
+        case SET_HISTORY_ERROR : 
             return {
                 ...state,
                 historyError: payload
